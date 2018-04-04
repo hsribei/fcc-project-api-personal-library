141ca5cf1ac3f5ec0d34662f9eab5d319e58e837
Helder S Ribeiro <hsribei.pub@gmail.com>
2018-04-04 17:03:07 -0400

    s/var/const/

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,11 @@
 
 "use strict";
 
-var expect = require("chai").expect;
-var MongoClient = require("mongodb").MongoClient;
-var ObjectId = require("mongodb").ObjectId;
-const MONGODB_CONNECTION_STRING = process.env.DB;
+require("dotenv").config();
+const expect = require("chai").expect;
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const MONGODB_CONNECTION_STRING = process.env.MONGO_URI;
 //Example connection: MongoClient.connect(MONGODB_CONNECTION_STRING, function(err, db) {});
 
 module.exports = function(app) {
@@ -23,7 +24,7 @@ module.exports = function(app) {
     })
 
     .post(function(req, res) {
-      var title = req.body.title;
+      const title = req.body.title;
       //response will contain new book object including atleast _id and title
     })
 
@@ -34,18 +35,18 @@ module.exports = function(app) {
   app
     .route("/api/books/:id")
     .get(function(req, res) {
-      var bookid = req.params.id;
+      const bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
 
     .post(function(req, res) {
-      var bookid = req.params.id;
-      var comment = req.body.comment;
+      const bookid = req.params.id;
+      const comment = req.body.comment;
       //json res format same as .get
     })
 
     .delete(function(req, res) {
-      var bookid = req.params.id;
+      const bookid = req.params.id;
       //if successful response will be 'delete successful'
     });
 };
